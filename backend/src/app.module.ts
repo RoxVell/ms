aac1afa04f8f87e40f8c56847f0026e1ec62f6fb
Anton <anton1.a@mail.com>
2021-06-17 19:24:02 +0300

    changes

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,26 +3,28 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { WindowsModule } from './windows/windows.module';
 import { UsersModule } from './users/users.module';
 import { ServicesModule } from './services/services.module';
-import { User } from './users/users.model';
 import { OperatorsModule } from './operators/operators.module';
 import { RolesModule } from './roles/roles.module';
-import { Role } from './roles/roles.model';
-import { Service } from "./services/service.model";
 import { AuthModule } from './auth/auth.module';
 import { ServiceTreeModule } from './service-tree/service-tree.module';
 import { TicketsModule } from './tickets/tickets.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     SequelizeModule.forRoot({
       dialect: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'root',
-      database: 'equeue',
+      host: process.env.POSTGRES_HOST || 'localhost',
+      port: Number(process.env.DB_PORT) || 5432,
+      username: process.env.POSTGRES_USERNAME || 'postgres',
+      password: process.env.POSTGRES_PASSWORD || 'root',
+      database: process.env.POSTGRES_DATABASE || 'equeue',
       models: [],
       autoLoadModels: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
     }),
     WindowsModule,
     UsersModule,
--- frontend/src/app/components/layouts/default-layout/default-layout.component.ts
+++ frontend/src/app/components/layouts/default-layout/default-layout.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class DefaultLayout implements OnInit {
   appitems = [
     {
-      label: 'Услуги',
+      label: 'Управление услугами',
       items: [
         {
           label: 'Услуги',
